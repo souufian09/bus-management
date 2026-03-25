686ad48438819cf5183457289ec2487aed9ece10
soufian tassi <soufiantassi69@gmail.com>
2026-03-25 19:35:57 +0100

    modif

--- user-service/UserService/UserService/DTOs/UpdateUserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserService.DTOs
-{
-    public class UpdateUserDto
-    {
-        public string Nom { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-
-    }
-}
